098331f20e17455d55243be1d8d13582d0781b19
Divyansh Singh <sdivyansh59@gmail.com>
2023-02-12 00:44:07 +0530

    Tests and Subtest in go adding subtest for armstrong number calculator

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -13,26 +13,58 @@ type TestCase struct {
 }
 
 func TestCalculeIsArmstrong(t *testing.T) {
-	testCase := TestCase {
-		value: 371,
-		expected: true,
-	}
 
-	testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-	if testCase.actual != testCase.expected {
-		t.Fail()
-	}
+	t.Run("should return true for 371", func(t *testing.T) {
+		testCase := TestCase {
+			value: 371,
+			expected: true,
+		}
+
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("should return true for 370", func(t *testing.T) {
+		testCase := TestCase {
+			value: 370,
+			expected: true,
+		}
+
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+	
 }
 
 
 func TestNegativeCalculeIsArmstrong(t *testing.T) {
-	testCase := TestCase {
-		value: 350,
-		expected: false,
-	}
 
-	testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-	if testCase.actual != testCase.expected {
-		t.Fail()
-	}
+	t.Run("should fail for caase 350", func(t *testing.T) {
+		testCase := TestCase {
+			value: 350,
+			expected: false,
+		}
+
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("should fail for caase 300", func(t *testing.T) {
+		testCase := TestCase {
+			value: 300,
+			expected: false,
+		}
+
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+	
 }
