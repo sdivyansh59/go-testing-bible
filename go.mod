7f896652212a28ce962a917e8b843df412bd3978
Divyansh Singh <sdivyansh59@gmail.com>
2023-02-16 23:59:03 +0530

    Testing HTTP service with HTTPTest

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/sdivyansh59/go-testing-bible
 
 go 1.19
 
-require gopkg.in/yaml.v3 v3.0.1
+require (
+	github.com/go-playground/assert/v2 v2.2.0
+	github.com/stretchr/testify v1.8.1
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// HomePage - our homepage
+func HomePage(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+
+// Setup - sets up our routes
+func Setup(){
+	http.HandleFunc("/",HomePage)
+}
+
 
 func main() {
-	fmt.Println("Testring Bible..")
+	// fmt.Println("Testring Bible..")
+	Setup()
+	log.Fatal(http.ListenAndServe(":10000",nil))
 }
